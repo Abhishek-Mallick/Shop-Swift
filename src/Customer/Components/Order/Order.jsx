465b39cf119461f6fbb16c718f644d479495d3ba
Abhishek Mallick <mallickabhishek97@gmail.com>
2023-12-18 01:43:27 +0530

    Refactor Order and OrderCard components

--- src/Customer/Components/Order/Order.jsx
+++ src/Customer/Components/Order/Order.jsx
@@ -11,7 +11,7 @@ const orderStatus = [
 
 const Order = () => {
   return (
-    <div>
+    <div className="px:5 lg:px-20">
       <Grid container sx={{ justifyContent: "space-between" }}>
         <Grid item xs={2.5}>
           <div className="h-auto shadow-lg bg-white p-5 sticky top-5">
@@ -37,8 +37,10 @@ const Order = () => {
             </div>
           </div>
         </Grid>
-        <Grid item>
-            <OrderCard />
+        <Grid item xs={9}>
+            <div className="space-y-2">
+                {[1,1,1,1].map((item) => <OrderCard />)}
+            </div>
         </Grid>
       </Grid>
     </div>
