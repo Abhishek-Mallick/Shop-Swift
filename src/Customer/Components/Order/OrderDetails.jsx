155eb1f4049cc29b44429794297d07932e642c52
Abhishek Mallick <mallickabhishek97@gmail.com>
2023-12-18 02:24:20 +0530

    Add product rating and review feature

--- src/Customer/Components/Order/OrderDetails.jsx
+++ src/Customer/Components/Order/OrderDetails.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import AddressCard from '../AddressCard/AddressCard';
 import OrderTracker from './OrderTracker';
+import { Box, Grid } from '@mui/material';
+import { deepPurple } from '@mui/material/colors';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
 
 const OrderDetails = () => {
     return (
@@ -12,6 +15,37 @@ const OrderDetails = () => {
             <div className='py-20'>
                 <OrderTracker activeStep={2}/> 
             </div>
+
+            <Grid container className='space-y-5'>
+                {[1,1,1,1].map((item) => 
+                <Grid item container className='shadow-xl rounded-md p-5 border' sx={{alignItems:"center",justifyContent:'space-between'}}>
+                    <Grid item xs={6}>
+                        <div className='flex items-center space-x-4'>
+                            <img className='w-[5rem] h-[5rem] object-cover object-top' src="https://rukminim1.flixcart.com/image/612/612/l5h2xe80/kurta/x/6/n/xl-kast-tile-green-majestic-man-original-imagg4z33hu4kzpv.jpeg?q=70" alt="" />
+                        
+                            <div className='space-y-2 ml-5'>
+                                <p className='font-semibold'>Title</p>
+                                <p className='space-x-4 opacity-50 text-xs font-bold'>
+                                    <span>Color: Black</span>
+                                    <span>Size: Medium</span>
+                                </p>
+                                <p>Seller: Brand Name</p>
+                                <p>₹4000</p>
+                            </div>
+                        </div>
+                    </Grid>
+
+                    <Grid item>
+                        <Box sx={{color: deepPurple[500]}}>
+                            <StarBorderIcon sx={{fontSize: "2rem"}} className='px-2 text-5xl'/>
+                            <span>Rate & Review Product</span>
+                        </Box>
+                    </Grid>
+                </Grid>
+                )}
+                
+            </Grid>
+
         </div>
     )
 }
